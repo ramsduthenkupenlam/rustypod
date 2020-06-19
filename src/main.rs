859a3d4a133cd03e3081fe54bf292ef694031d12
John Ramsden <johnramsden@riseup.net>
2020-06-18 19:57:07 -0700

    Add clap and CLI boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("rustpod")
+        .version("0.0.0")
+        .author("John Ramsden and Joe Puthenkulam")
+        .about("Podcast downloader")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Sets a custom config file")
+                .takes_value(true),
+        )
+        .get_matches();
+
 }
