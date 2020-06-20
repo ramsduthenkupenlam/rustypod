477a6dc9158a5d7f1b3bcae33807e0949bfe0cc4
John Ramsden <johnramsden@riseup.net>
2020-06-19 17:19:24 -0700

    Add feed-rs, reqwest dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate clap;
+extern crate feed_rs;
 
 use clap::{App, Arg};
+use reqwest::blocking;
+use feed_rs::parser;
 
 fn main() {
     let matches = App::new("rustpod")
@@ -17,4 +20,15 @@ fn main() {
         )
         .get_matches();
 
+    let body = blocking::get("https://pythonbytes.fm/episodes/rss")
+        .unwrap().text().unwrap();
+
+    let feed_from_xml = parser::parse(body.as_bytes()).unwrap();
+
+    for e in feed_from_xml.entries {
+        println!("Date: {}", e.published.unwrap());
+        println!("Title: {}", e.title.unwrap().content);
+        println!("Link: {}", e.content.unwrap().src.unwrap().href);
+        println!("{}", "");
+    }
 }
